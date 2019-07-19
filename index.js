62f8504195c91b9a4519a2778c19a55d06ebe05b
Katherine Wang <kwang004@gmail.com>
2019-07-19 14:42:37 -0700

    Add route for photos server

--- index.js
+++ index.js
@@ -14,50 +14,46 @@ app.use(morgan());
 app.use('/:listingID', express.static('./public/dist'));
 
 app.all('/listing/desc/:listingID', (req, res) => {
-  console.log('redirecting to listing server');
-  
   apiProxy.web(req, res, {
     target: serverListing
   });
 })
 
 app.all('/listing/amenity/:listingID', (req, res) => {
-  console.log('redirecting to listing server');
-  
   apiProxy.web(req, res, {
     target: serverListing
   });
 })
 
 app.all('/listing/:listingID', (req, res) => {
-  console.log('redirecting to calendar server');
-  
   apiProxy.web(req, res, {
     target: serverCalendar
   });
 })
 
 app.all('/custom/month', (req, res) => {
-  console.log('redirecting to calendar server');
-  
   apiProxy.web(req, res, {
     target: serverCalendar
   });
 })
 
 app.all('/reserved/month', (req, res) => {
-  console.log('redirecting to calendar server');
-  
   apiProxy.web(req, res, {
     target: serverCalendar
   });
 })
 
 app.all('/api/listings/photos/:listingID', (req, res) => {
-  console.log('redirecting to photos server');
-  
   apiProxy.web(req, res, {
     target: serverPhotos
   });
 })
+
+app.all("/api/listings/photos/initial/:listingID", function(req, res) {
+  apiProxy.web(req, res, 
+    {target: serverPhotos
+  });
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
