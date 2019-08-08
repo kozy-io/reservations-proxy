dec47e058e398b0a718ffe36d09e0516af803806
Shane Zhao <s.x.zhao@gmail.com>
2019-08-07 18:49:46 -0700

    update

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const httpProxy = require('http-proxy');
 require('newrelic');
 const apiProxy = httpProxy.createProxyServer();
-const serverPhotos = 'http://3.14.5.145';
-const serverCalendar = 'http://18.188.235.153';
-const serverListing = 'http://18.221.218.103';
-const serverReviews = 'http://ec2-13-57-195-146.us-west-1.compute.amazonaws.com';
+const serverReservations = 'http://13.52.104.167:3006';
 const app = express();
 const port = 3005;
 const bodyParser = require('body-parser');
@@ -14,54 +11,18 @@ app.use(bodyParser());
 app.use(morgan());
 app.use('/:listingID', express.static('./public/dist'));
 
-
 app.all('/api/reservations/:listingID', (req, res) => {
   apiProxy.web(req, res, {
-    target: 'http://127.0.0.1:3006'
+    target: serverReservations
   });
 })
 
-// app.all('/listing/desc/:listingID', (req, res) => {
+// Connect to reservations locally
+// app.all('/api/reservations/:listingID', (req, res) => {
 //   apiProxy.web(req, res, {
-//     target: serverListing
+//     target: 'http://127.0.0.1:3006'
 //   });
 // })
-
-// app.all('/listing/amenity/:listingID', (req, res) => {
-//   apiProxy.web(req, res, {
-//     target: serverListing
-//   });
-// })
-
-// app.all('/listing/:listingID', (req, res) => {
-//   apiProxy.web(req, res, {
-//     target: serverCalendar
-//   });
-// })
-
-// app.all('/custom/month', (req, res) => {
-//   apiProxy.web(req, res, {
-//     target: serverCalendar
-//   });
-// })
-
-// app.all('/reserved/month', (req, res) => {
-//   apiProxy.web(req, res, {
-//     target: serverCalendar
-//   });
-// })
-
-// app.all('/api/listings/photos/:listingID', (req, res) => {
-//   apiProxy.web(req, res, {
-//     target: serverPhotos
-//   });
-// })
-
-// app.all("/api/listings/photos/initial/:listingID", function(req, res) {
-//   apiProxy.web(req, res, 
-//     {target: serverPhotos
-//   });
-// });
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
